59e6dc8b491e83b4851fd38847dc4dd658151c53
Genaro Melgar <gjmg1612@gmail.com>
2022-01-27 11:22:13 -0600

    terminado responsive sección de tutores

--- src/sections/IndexTutorsSection.jsx
+++ src/sections/IndexTutorsSection.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexTutorsSection = () => {
   return (
-    <div className="h-screen grid justify-center gap-y-2 place-content-center">
-      <div className="container text-center">
-        <h1 className="text-3xl mb-10">Conoce a tus tutores</h1>
-        <StaticImage src="./genaro.jpg" alt="No encontrada" className="rounded-full w-1/2"/>
-        <h1 className="text-2xl">Genaro Melgar</h1>
-        <h1 className="text-base text-current">
-          Estudiante de Ciencia de Datos en el Instituto Politécnco Nacional
-        </h1>
-        <StaticImage src="./ivan.jpg" alt="No encontrada" className="rounded-full w-1/2"/>
-        <h1 className="text-2xl">Iván Martínez</h1>
-        <h1 className="text-base text-current">
-          Estudiante de Matemáticas Aplicadas y Computación en la UNAM
-        </h1>
+    <div className="mt-10 grid text-center justify-center gap-y-2 h-full sm:h-screen">
+      <h1 className="text-3xl mb-10">Conoce a tus tutores</h1>
+      <div className="grid sm:flex">
+        <div className="pl-10 pr-10 pb-10 w-full sm:w-1/2 ">
+          <StaticImage
+            src="./genaro.jpg"
+            alt="No encontrada"
+            className="rounded-full w-1/2"
+          />
+          <h1 className="text-2xl mt-5 mb-0">Genaro Melgar</h1>
+          <h1 className="text-base font-thin">
+            Estudiante de Ciencia de Datos en el Instituto Politécnco Nacional
+          </h1>
+        </div>
+        <div className="pl-10 pr-10 pb-10 w-full sm:w-1/2 ">
+          <StaticImage
+            src="./ivan.jpg"
+            alt="No encontrada"
+            className="rounded-full w-1/2"
+          />
+          <h1 className="text-2xl mt-5 mb-0">Iván Martínez</h1>
+          <h1 className="text-base font-thin">
+            Estudiante de Matemáticas Aplicadas y Computación en la UNAM
+          </h1>
+        </div>
       </div>
     </div>
   )
